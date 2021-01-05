25ab4dc76e011360373d30909eab1097200e678e
Diego <diegoteixeirapereira@hotmail.com>
2021-01-05 15:36:43 -0300

    criação do arquivo classe consultarpessoacontroller

---
+++ AppJSF/src/main/java/br/com/java/pessoa/controller/ConsultarPessoaController.java
@@ -0,0 +1,56 @@
+package br.com.java.pessoa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.java.model.PessoaModel;
+import br.com.java.repository.PessoaRepository;
+
+@Named(value = "consultarPessoaController")
+@ViewScoped
+public class ConsultarPessoaController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	transient private PessoaModel pessoaModel;
+
+	@Produces
+	private List<PessoaModel> pessoas;
+
+	@Inject
+	transient private PessoaRepository pessoaRepository;
+
+	public List<PessoaModel> getPessoas() {
+		return pessoas;
+	}
+
+	public void setPessoas(List<PessoaModel> pessoas) {
+		this.pessoas = pessoas;
+	}
+
+	public PessoaModel getPessoaModel() {
+		return pessoaModel;
+	}
+
+	public void setPessoaModel(PessoaModel pessoaModel) {
+		this.pessoaModel = pessoaModel;
+	}
+
+	/***
+	 * CARREGA AS PESSOAS NA INICIALIZAÇÃO
+	 */
+	@PostConstruct
+	public void init() {
+
+		// RETORNAR AS PESSOAS CADASTRADAS
+		this.pessoas = pessoaRepository.GetPessoas();
+	}
+
+}
