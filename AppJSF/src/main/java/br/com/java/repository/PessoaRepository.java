9b0c332f650fedb70e9307a6fa48e06541846c2e
Diego <diegoteixeirapereira@hotmail.com>
2021-01-05 15:34:55 -0300

    alteração no arquivo classe PessoaRepository adicionado metodo listar pessoas

--- AppJSF/src/main/java/br/com/java/repository/PessoaRepository.java
+++ AppJSF/src/main/java/br/com/java/repository/PessoaRepository.java
@@ -1,11 +1,16 @@
 package br.com.java.repository;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 
 import br.com.java.model.PessoaModel;
+import br.com.java.model.UsuarioModel;
 import br.com.java.repository.entity.PessoaEntity;
 import br.com.java.repository.entity.UsuarioEntity;
 import br.com.java.uteis.Uteis;
@@ -38,6 +43,57 @@ public class PessoaRepository {
 		pessoaEntity.setUsuarioEntity(usuarioEntity);
  
 		entityManager.persist(pessoaEntity);
+ 
+	}
+	
+	/***
+	 * MÉTODO PARA CONSULTAR A PESSOA
+	 * @return
+	 */
+	public List<PessoaModel> GetPessoas(){
+ 
+		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
+ 
+		entityManager =  Uteis.JpaEntityManager();
+ 
+		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
+ 
+		@SuppressWarnings("unchecked")
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>)query.getResultList();
+ 
+		PessoaModel pessoaModel = null;
+ 
+		for (PessoaEntity pessoaEntity : pessoasEntity) {
+ 
+			pessoaModel = new PessoaModel();
+			pessoaModel.setCodigo(pessoaEntity.getCodigo());
+			pessoaModel.setDataCadastro(pessoaEntity.getDataCadastro());
+			pessoaModel.setEmail(pessoaEntity.getEmail());
+			pessoaModel.setEndereco(pessoaEntity.getEndereco());
+			pessoaModel.setNome(pessoaEntity.getNome());
+ 
+			if(pessoaEntity.getOrigemCadastro().equals("X"))
+				pessoaModel.setOrigemCadastro("XML");
+			else
+				pessoaModel.setOrigemCadastro("INPUT");
+ 
+ 
+			if(pessoaEntity.getSexo().equals("M"))
+				pessoaModel.setSexo("Masculino");
+			else
+				pessoaModel.setSexo("Feminino");
+ 
+			UsuarioEntity usuarioEntity =  pessoaEntity.getUsuarioEntity();			
+ 
+			UsuarioModel usuarioModel = new UsuarioModel();
+			usuarioModel.setUsuario(usuarioEntity.getUsuario());
+ 
+			pessoaModel.setUsuarioModel(usuarioModel);
+ 
+			pessoasModel.add(pessoaModel);
+		}
+ 
+		return pessoasModel;
  
 	}
 
