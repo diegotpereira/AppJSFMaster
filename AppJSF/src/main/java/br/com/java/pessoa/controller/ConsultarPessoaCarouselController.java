bff6fffbdced4461a43b8d924ab4efb0bcb9256c
Diego <diegoteixeirapereira@hotmail.com>
2021-01-05 16:34:40 -0300

    criação do arquivo classe consultarpessoacarouselcontroller

---
+++ AppJSF/src/main/java/br/com/java/pessoa/controller/ConsultarPessoaCarouselController.java
@@ -0,0 +1,37 @@
+package br.com.java.pessoa.controller;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.inject.Produces;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import br.com.java.model.PessoaModel;
+import br.com.java.repository.PessoaRepository;
+
+@Named(value = "consultarPessoaCarouselController")
+@ViewScoped
+public class ConsultarPessoaCarouselController implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	transient private PessoaRepository pessoaRepository;
+
+	@Produces
+	private List<PessoaModel> pessoas;
+
+	public List<PessoaModel> getPessoas() {
+		return pessoas;
+	}
+
+	@PostConstruct
+	private void init() {
+
+		this.pessoas = pessoaRepository.GetPessoas();
+	}
+
+}
