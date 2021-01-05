607b16e801f87f5e8783caa0e646bf0efba9298a
Diego <diegoteixeirapereira@hotmail.com>
2021-01-05 16:06:48 -0300

    alteração no arquivo classe PessoaRepository adicionado metodos de alteração de registro e consulta por codigo

--- AppJSF/src/main/java/br/com/java/repository/PessoaRepository.java
+++ AppJSF/src/main/java/br/com/java/repository/PessoaRepository.java
@@ -16,20 +16,21 @@ import br.com.java.repository.entity.UsuarioEntity;
 import br.com.java.uteis.Uteis;
 
 public class PessoaRepository {
-	
+
 	@Inject
 	PessoaEntity pessoaEntity;
- 
+
 	EntityManager entityManager;
- 
+
 	/***
 	 * MÉTODO RESPONSÁVEL POR SALVAR UMA NOVA PESSOA
+	 * 
 	 * @param pessoaModel
 	 */
-	public void SalvarNovoRegistro(PessoaModel pessoaModel){
- 
-		entityManager =  Uteis.JpaEntityManager();
- 
+	public void SalvarNovoRegistro(PessoaModel pessoaModel) {
+
+		entityManager = Uteis.JpaEntityManager();
+
 		pessoaEntity = new PessoaEntity();
 		pessoaEntity.setDataCadastro(LocalDateTime.now());
 		pessoaEntity.setEmail(pessoaModel.getEmail());
@@ -37,64 +38,97 @@ public class PessoaRepository {
 		pessoaEntity.setNome(pessoaModel.getNome());
 		pessoaEntity.setOrigemCadastro(pessoaModel.getOrigemCadastro());
 		pessoaEntity.setSexo(pessoaModel.getSexo());
- 
-		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class, pessoaModel.getUsuarioModel().getCodigo()); 
- 
+
+		UsuarioEntity usuarioEntity = entityManager.find(UsuarioEntity.class,
+				pessoaModel.getUsuarioModel().getCodigo());
+
 		pessoaEntity.setUsuarioEntity(usuarioEntity);
- 
+
 		entityManager.persist(pessoaEntity);
- 
+
 	}
-	
+
 	/***
 	 * MÉTODO PARA CONSULTAR A PESSOA
+	 * 
 	 * @return
 	 */
-	public List<PessoaModel> GetPessoas(){
- 
+	public List<PessoaModel> GetPessoas() {
+
 		List<PessoaModel> pessoasModel = new ArrayList<PessoaModel>();
- 
-		entityManager =  Uteis.JpaEntityManager();
- 
+
+		entityManager = Uteis.JpaEntityManager();
+
 		Query query = entityManager.createNamedQuery("PessoaEntity.findAll");
- 
+
 		@SuppressWarnings("unchecked")
-		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>)query.getResultList();
- 
+		Collection<PessoaEntity> pessoasEntity = (Collection<PessoaEntity>) query.getResultList();
+
 		PessoaModel pessoaModel = null;
- 
+
 		for (PessoaEntity pessoaEntity : pessoasEntity) {
- 
+
 			pessoaModel = new PessoaModel();
 			pessoaModel.setCodigo(pessoaEntity.getCodigo());
 			pessoaModel.setDataCadastro(pessoaEntity.getDataCadastro());
 			pessoaModel.setEmail(pessoaEntity.getEmail());
 			pessoaModel.setEndereco(pessoaEntity.getEndereco());
 			pessoaModel.setNome(pessoaEntity.getNome());
- 
-			if(pessoaEntity.getOrigemCadastro().equals("X"))
+
+			if (pessoaEntity.getOrigemCadastro().equals("X"))
 				pessoaModel.setOrigemCadastro("XML");
 			else
 				pessoaModel.setOrigemCadastro("INPUT");
- 
- 
-			if(pessoaEntity.getSexo().equals("M"))
+
+			if (pessoaEntity.getSexo().equals("M"))
 				pessoaModel.setSexo("Masculino");
 			else
 				pessoaModel.setSexo("Feminino");
- 
-			UsuarioEntity usuarioEntity =  pessoaEntity.getUsuarioEntity();			
- 
+
+			UsuarioEntity usuarioEntity = pessoaEntity.getUsuarioEntity();
+
 			UsuarioModel usuarioModel = new UsuarioModel();
 			usuarioModel.setUsuario(usuarioEntity.getUsuario());
- 
+
 			pessoaModel.setUsuarioModel(usuarioModel);
- 
+
 			pessoasModel.add(pessoaModel);
 		}
- 
+
 		return pessoasModel;
- 
+
+	}
+
+	/***
+	 * CONSULTA UMA PESSOA CADASTRADA PELO CÓDIGO
+	 * 
+	 * @param codigo
+	 * @return
+	 */
+	private PessoaEntity GetPessoa(int codigo) {
+
+		entityManager = Uteis.JpaEntityManager();
+
+		return entityManager.find(PessoaEntity.class, codigo);
+	}
+
+	/***
+	 * ALTERA UM REGISTRO CADASTRADO NO BANCO DE DADOS
+	 * 
+	 * @param pessoaModel
+	 */
+	public void AlterarRegistro(PessoaModel pessoaModel) {
+
+		entityManager = Uteis.JpaEntityManager();
+
+		PessoaEntity pessoaEntity = this.GetPessoa(pessoaModel.getCodigo());
+
+		pessoaEntity.setEmail(pessoaModel.getEmail());
+		pessoaEntity.setEndereco(pessoaModel.getEndereco());
+		pessoaEntity.setNome(pessoaModel.getNome());
+		pessoaEntity.setSexo(pessoaModel.getSexo());
+
+		entityManager.merge(pessoaEntity);
 	}
 
 }
